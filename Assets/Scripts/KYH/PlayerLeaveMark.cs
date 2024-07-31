f4543616c713f4719eb4ff30dded2d7ad44d8fb0
Fststory <fststory@naver.com>
2024-08-01 00:59:32 +0900

    240731 찐막 branch

--- Assets/Scripts/KYH/PlayerLeaveMark.cs
+++ Assets/Scripts/KYH/PlayerLeaveMark.cs
@@ -10,18 +10,19 @@ public class PlayerLeaveMark : MonoBehaviour
         뛰어다닐 때 흔적을 남김
      */
 
-    public Transform player;
-    public GameObject traceMarkPrefab;
+    public Transform player;    // 플레이어 트랜스폼 넣기
+    public GameObject traceMarkPrefab;  // TraceMark 프리팹 넣기
     float currentTime = 0;
     public float markCycle = 0.5f;  // 흔적을 남기는 주기
+    public PlayerController playerController;   // 플레이어에 달려있는 PlayerController 컴포넌트 넣어줘야 됨
 
 
     void Update()
     {
-        //if (playerController.run)
-        //{
+        if (playerController.run)
+        {
             LeaveMark();
-        //}
+        }
     }
 
     public void LeaveMark() // 플레이어가 뛰는 상태일 때 사용
