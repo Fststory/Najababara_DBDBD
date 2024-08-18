c5e1ea6ebfd775f9aa4a55461b9cc806416e9993
goranirago <zimi749@naver.com>
2024-08-18 19:01:36 +0900

    240818 피격 소리 추가

--- Assets/Scripts/HJM/HitSound.cs
+++ Assets/Scripts/HJM/HitSound.cs
@@ -15,10 +15,12 @@ public class HitSound : MonoBehaviour
     {
         // 이 게임 오브젝트에 붙어있는 오디오 소스를 가져옴
         audioSource = GetComponent<AudioSource>();
-
+        print("111111111111");
         // 오디오 소스가 없으면 추가
         if (audioSource == null)
         {
+            print("22222222");
+
             audioSource = gameObject.AddComponent<AudioSource>();
         }
     }
@@ -26,20 +28,43 @@ public class HitSound : MonoBehaviour
     // 애니메이션 이벤트
     public void PlayHitSound()
     {
-        // 비명 소리와 맞는 소리를 각각 무작위로 선택하여 재생
-
         // 비명 소리 재생
         if (screamClips.Length > 0)
         {
+            print("3333333333");
+
             int randomScreamIndex = Random.Range(0, screamClips.Length);
+            print("4444444");
+
             audioSource.PlayOneShot(screamClips[randomScreamIndex]);
+            print("5555555");
+
+            Debug.Log("비명 소리 재생됨");
+        }
+        else
+        {
+            Debug.LogWarning("비명 소리 클립이 없습니다.");
         }
 
         // 맞는 소리 재생
         if (hitClips.Length > 0)
         {
+            print("666666");
+
             int randomHitIndex = Random.Range(0, hitClips.Length);
+
+            print("77777777");
+
             audioSource.PlayOneShot(hitClips[randomHitIndex]);
+
+            print("888888888");
+
+            Debug.Log("맞는 소리 재생됨");
+        }
+        else
+        {
+
+            Debug.LogWarning("맞는 소리 클립이 없습니다.");
         }
     }
 }
