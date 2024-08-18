d2efa50f60afb4ccfeb1bb01d7f5e6e1eba653d3
Fststory <fststory@naver.com>
2024-08-18 17:53:08 +0900

    240818 branch

--- Assets/Scripts/KYH/AttackTrigger.cs
+++ Assets/Scripts/KYH/AttackTrigger.cs
@@ -8,8 +8,11 @@ public class AttackTrigger : MonoBehaviour
     public Animator playerAnim;
     public BoxCollider boxCol;
 
-    public AudioClip[] attackSounds;  // 타격음 클립 배열
+    public AudioClip[] bluntSounds;  // 타격음 클립 배열
+    public AudioClip[] attackSounds;  // 휘두르는 소리 클립 배열
 
+    public AudioSource bluntAudioSource;
+    public AudioSource attackAudioSource;
 
     void Start()
     {
@@ -41,7 +44,7 @@ public class AttackTrigger : MonoBehaviour
                 playerFSM.pyState = PlayerFSM.PlayerState.Injured;
                 print("Hit01");
                 playerAnim.SetTrigger("Hit01");
-                
+                PlayRandomBluntSound();   
             }
             else if (playerFSM.pyState == PlayerFSM.PlayerState.Injured)
             {
@@ -49,9 +52,47 @@ public class AttackTrigger : MonoBehaviour
                 print("Hit02");
                 playerAnim.SetTrigger("Hit02");
                 playerAnim.SetBool("Dying", true);
+                PlayRandomBluntSound();
             }
         }
     }
 
+    void PlayRandomBluntSound()     // 트리거에 플레이어 맞았을 때 재생
+    {
+        print(111111111);
+        if (bluntSounds.Length == 0) return;
+        print(222222222);
+        AudioClip selectedClip = GetRandomClip(bluntSounds);
+        if (selectedClip == null)
+        {
+            Debug.LogWarning("Selected blunt clip is null, skipping playback.");
+            return;
+        }
 
+        Debug.Log($"Playing clip: {selectedClip.name} on {bluntAudioSource.name}");
+        bluntAudioSource.PlayOneShot(selectedClip);  // PlayOneShot을 사용하여 클립 재생
+    }
+
+    void PlayRandomAttackSound()    // 공격 애니메이션 종료 시점 이벤트
+    {
+        if (attackSounds.Length == 0) return;
+
+        AudioClip selectedClip = GetRandomClip(attackSounds);
+        if (selectedClip == null)
+        {
+            Debug.LogWarning("Selected clip is null, skipping playback.");
+            return;
+        }
+
+        Debug.Log($"Playing clip: {selectedClip.name} on {attackAudioSource.name}");
+        attackAudioSource.PlayOneShot(selectedClip);  // PlayOneShot을 사용하여 클립 재생
+    }
+
+    AudioClip GetRandomClip(AudioClip[] audioClips)
+    {
+        if (audioClips.Length == 0) return null;
+        int index = Random.Range(0, audioClips.Length);
+        Debug.Log($"Selected clip index: {index}");
+        return audioClips[index];
+    }
 }
